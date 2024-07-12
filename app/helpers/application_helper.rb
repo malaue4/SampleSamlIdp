401138754b15346724117dbdf86c71ead888260e
Martin Laursen <martin@assemblyvoting.com>
2024-07-12 08:48:14 +0200

    dry up link generation a bit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 module ApplicationHelper
   include Pagy::Frontend
 
+  def link_label(link)
+    case link
+    in Class if link < ApplicationRecord
+      link.model_name.human(count: 2)
+    in ApplicationRecord
+      link.model_name.human(count: 2)
+    else
+      link.to_s.humanize
+    end
+  end
 end

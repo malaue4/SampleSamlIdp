9b3852ff68731830c669b54b6c58f4c8c815119e
Martin Laursen <malaue4@gmail.com>
2025-09-26 20:37:08 +0200

    Refactor SAML request tests to replace hardcoded certificates with dynamic generation, improve signature verification, and clean up test cases.

--- app/models/saml/request.rb
+++ app/models/saml/request.rb
@@ -98,11 +98,7 @@ module Saml
 
     def verify_signature(certificate)
       signed_document = Xmldsig::SignedDocument.new(raw_request)
-      signed_document.validate(certificate) do |it|
-        pp it
-      end
-      pp signed_document.signatures
-      pp signed_document.signatures.first.instance_variable_get(:@errors)
+      signed_document.validate(certificate)
     end
 
     # The Extensions element of the request. This element contains optional metadata or additional elements
--- test/models/saml/request_test.rb
+++ test/models/saml/request_test.rb
@@ -14,7 +14,7 @@ module Saml
     DEFLATED_AUTHN_REQUEST_XML = Request::Encoding.encode(Request::Compression.deflate(AUTHN_REQUEST_XML))
     UNDEFLATED_AUTHN_REQUEST_XML = Request::Encoding.encode(AUTHN_REQUEST_XML)
 
-    SIGNED_XML_REQUEST_XML = <<~XML
+    UNSIGNED_XML_REQUEST_XML = <<~XML
       <samlp:AuthnRequest xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol" xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" ID="pfx41d8ef22-e612-8c50-9960-1b16f15741b3" Version="2.0" ProviderName="SP test" IssueInstant="2014-07-16T23:52:45Z" Destination="http://idp.example.com/SSOService.php" ProtocolBinding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" AssertionConsumerServiceURL="http://sp.example.com/demo1/index.php?acs">
         <saml:Issuer>http://sp.example.com/demo1/metadata.php</saml:Issuer>
         <ds:Signature xmlns:ds="http://www.w3.org/2000/09/xmldsig#">
@@ -27,13 +27,13 @@ module Saml
                 <ds:Transform Algorithm="http://www.w3.org/2001/10/xml-exc-c14n#"/>
               </ds:Transforms>
               <ds:DigestMethod Algorithm="http://www.w3.org/2000/09/xmldsig#sha1"/>
-              <ds:DigestValue>yJN6cXUwQxTmMEsPesBP2NkqYFI=</ds:DigestValue>
+              <ds:DigestValue></ds:DigestValue>
             </ds:Reference>
           </ds:SignedInfo>
-          <ds:SignatureValue>g5eM9yPnKsmmE/Kh2qS7nfK8HoF6yHrAdNQxh70kh8pRI4KaNbYNOL9sF8F57Yd+jO6iNga8nnbwhbATKGXIZOJJSugXGAMRyZsj/rqngwTJk5KmujbqouR1SLFsbo7Iuwze933EgefBbAE4JRI7V2aD9YgmB3socPqAi2Qf97E=</ds:SignatureValue>
+          <ds:SignatureValue></ds:SignatureValue>
           <ds:KeyInfo>
             <ds:X509Data>
-              <ds:X509Certificate>MIICajCCAdOgAwIBAgIBADANBgkqhkiG9w0BAQQFADBSMQswCQYDVQQGEwJ1czETMBEGA1UECAwKQ2FsaWZvcm5pYTEVMBMGA1UECgwMT25lbG9naW4gSW5jMRcwFQYDVQQDDA5zcC5leGFtcGxlLmNvbTAeFw0xNDA3MTcwMDI5MjdaFw0xNTA3MTcwMDI5MjdaMFIxCzAJBgNVBAYTAnVzMRMwEQYDVQQIDApDYWxpZm9ybmlhMRUwEwYDVQQKDAxPbmVsb2dpbiBJbmMxFzAVBgNVBAMMDnNwLmV4YW1wbGUuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC7vU/6R/OBA6BKsZH4L2bIQ2cqBO7/aMfPjUPJPSn59d/f0aRqSC58YYrPuQODydUABiCknOn9yV0fEYm4bNvfjroTEd8bDlqo5oAXAUAI8XHPppJNz7pxbhZW0u35q45PJzGM9nCv9bglDQYJLby1ZUdHsSiDIpMbGgf/ZrxqawIDAQABo1AwTjAdBgNVHQ4EFgQU3s2NEpYx7wH6bq7xJFKa46jBDf4wHwYDVR0jBBgwFoAU3s2NEpYx7wH6bq7xJFKa46jBDf4wDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQQFAAOBgQCPsNO2FG+zmk5miXEswAs30E14rBJpe/64FBpM1rPzOleexvMgZlr0/smF3P5TWb7H8Fy5kEiByxMjaQmml/nQx6qgVVzdhaTANpIE1ywEzVJlhdvw4hmRuEKYqTaFMLez0sRL79LUeDxPWw7Mj9FkpRYT+kAGiFomHop1nErV6Q==</ds:X509Certificate>
+              <ds:X509Certificate></ds:X509Certificate>
             </ds:X509Data>
           </ds:KeyInfo>
         </ds:Signature>
@@ -42,13 +42,7 @@ module Saml
           <saml:AuthnContextClassRef>urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport</saml:AuthnContextClassRef>
         </samlp:RequestedAuthnContext>
       </samlp:AuthnRequest>
-          XML
-
-    CERTIFICATE = <<CERTIFICATE
------BEGIN CERTIFICATE-----
-MIICajCCAdOgAwIBAgIBADANBgkqhkiG9w0BAQQFADBSMQswCQYDVQQGEwJ1czETMBEGA1UECAwKQ2FsaWZvcm5pYTEVMBMGA1UECgwMT25lbG9naW4gSW5jMRcwFQYDVQQDDA5zcC5leGFtcGxlLmNvbTAeFw0xNDA3MTcwMDI5MjdaFw0xNTA3MTcwMDI5MjdaMFIxCzAJBgNVBAYTAnVzMRMwEQYDVQQIDApDYWxpZm9ybmlhMRUwEwYDVQQKDAxPbmVsb2dpbiBJbmMxFzAVBgNVBAMMDnNwLmV4YW1wbGUuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC7vU/6R/OBA6BKsZH4L2bIQ2cqBO7/aMfPjUPJPSn59d/f0aRqSC58YYrPuQODydUABiCknOn9yV0fEYm4bNvfjroTEd8bDlqo5oAXAUAI8XHPppJNz7pxbhZW0u35q45PJzGM9nCv9bglDQYJLby1ZUdHsSiDIpMbGgf/ZrxqawIDAQABo1AwTjAdBgNVHQ4EFgQU3s2NEpYx7wH6bq7xJFKa46jBDf4wHwYDVR0jBBgwFoAU3s2NEpYx7wH6bq7xJFKa46jBDf4wDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQQFAAOBgQCPsNO2FG+zmk5miXEswAs30E14rBJpe/64FBpM1rPzOleexvMgZlr0/smF3P5TWb7H8Fy5kEiByxMjaQmml/nQx6qgVVzdhaTANpIE1ywEzVJlhdvw4hmRuEKYqTaFMLez0sRL79LUeDxPWw7Mj9FkpRYT+kAGiFomHop1nErV6Q==
------END CERTIFICATE-----
-CERTIFICATE
+    XML
 
     def setup
       # Do nothing
@@ -65,7 +59,7 @@ CERTIFICATE
 
     def test_parse_deflated
       request = Request.parse(DEFLATED_AUTHN_REQUEST_XML)
-      assert_equal UNDEFLATED_AUTHN_REQUEST_XML, request.raw_request
+      assert_equal AUTHN_REQUEST_XML, request.raw_request
     end
 
     def test_issuer_entity_id
@@ -128,9 +122,46 @@ CERTIFICATE
     end
 
     def test_verify_signature
-      req = Request.parse SIGNED_XML_REQUEST_XML
+      private_key = OpenSSL::PKey::RSA.new(2048)
+      certificate = generate_self_signed_certificate(private_key)
+      assert certificate.check_private_key private_key
+      signed_request = Xmldsig::SignedDocument.new(UNSIGNED_XML_REQUEST_XML).sign(private_key)
+      assert_kind_of String, signed_request
+      req = Request.parse signed_request
 
-      assert req.verify_signature(OpenSSL::X509::Certificate.new(CERTIFICATE))
+      assert req.verify_signature(certificate)
     end
+    
+    private
+
+      # Generates a self-signed certificate.
+      #
+      # @param private_key [OpenSSL::PKey]
+      # @return [OpenSSL::X509::Certificate]
+      def generate_self_signed_certificate(private_key)
+        cert = OpenSSL::X509::Certificate.new
+        cert.version = 2
+        cert.subject = OpenSSL::X509::Name.new([
+                                                 ["C", "US"],
+                                                 ["ST", "California"],
+                                                 ["L", "San Francisco"],
+                                                 ["O", "Test Organization"],
+                                                 ["CN", "test.example.com"]
+                                               ])
+        cert.issuer = cert.subject
+        cert.public_key = private_key.public_key
+        cert.not_before = 1.day.ago
+        cert.not_after = 1.year.from_now
+
+        ef = OpenSSL::X509::ExtensionFactory.new
+        ef.subject_certificate = cert
+        ef.issuer_certificate = cert
+
+        cert.add_extension(ef.create_extension("basicConstraints", "CA:TRUE", true))
+        cert.add_extension(ef.create_extension("keyUsage", "keyCertSign, cRLSign", true))
+        cert.add_extension(ef.create_extension("subjectKeyIdentifier", "hash"))
+
+        cert
+      end
   end
 end

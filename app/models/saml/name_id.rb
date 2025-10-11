3555965cb61c29165b518e15f5593c214de3da92
Martin Laursen <malaue4@gmail.com>
2025-10-11 04:18:46 +0200

    Add comprehensive tests for `NameId` and refactor model with attributes, default values, and XML handling

--- app/models/saml/name_id.rb
+++ app/models/saml/name_id.rb
@@ -3,17 +3,46 @@
 module Saml
   class NameId
     include ActiveModel::Model
+    include ActiveModel::Attributes
+    include ToXml
 
-    attr_accessor :value, :format, :sp_provided_id, :name_qualifier, :sp_name_qualifier
+    attribute :value, default: ""
+    attribute :format, default: "urn:oasis:names:tc:SAML:2.0:nameid-format:transient"
+    attribute :sp_provided_id, default: false
+    attribute :name_qualifier
+    attribute :sp_name_qualifier
 
     def self.parse(name_id_element)
       new(
-        value: name_id_element.text,
-        format: name_id_element.attribute("Format")&.value,
-        sp_provided_id: name_id_element.attribute("SPProvidedID")&.value,
-        name_qualifier: name_id_element.attribute("NameQualifier")&.value,
-        sp_name_qualifier: name_id_element.attribute("SPNameQualifier")&.value,
+        value: name_id_element.text&.squish,
+        format: name_id_element["Format"],
+        sp_provided_id: name_id_element["SPProvidedID"],
+        name_qualifier: name_id_element["NameQualifier"],
+        sp_name_qualifier: name_id_element["SPNameQualifier"],
       )
     end
+
+    private
+
+      def xml_namespace
+        "saml"
+      end
+
+      def xml_attributes
+        {
+          Format: format,
+          SPProvidedID: sp_provided_id,
+          NameQualifier: name_qualifier,
+          SPNameQualifier: sp_name_qualifier,
+        }.compact
+      end
+
+      def xml_content(builder)
+        builder.text(value)
+      end
+
+      def xml_element_name
+        "NameID"
+      end
   end
 end

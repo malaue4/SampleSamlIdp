07b3c48206938c56228b2fd2146e7e583bd727b2
Martin Laursen <malaue4@gmail.com>
2025-10-22 21:42:10 +0200

    .

--- test/controllers/saml/idp_controller_test.rb
+++ test/controllers/saml/idp_controller_test.rb
@@ -1,11 +1,22 @@
 require "test_helper"
 
-describe Saml::IdpController do
-  describe "#create" do
-    test "cancel_create_action" do
-      post "/saml/auth", params: { SAMLRequest: "<AuthnRequest xmlns=\"#{Saml::Namespaces::SAMLP}\"/>", submit: "Cancel" }
-      assert_match(/Status: urn:oasis:names:tc:SAML:2.0:status:Responder/, response.body)
-      assert_response :success
+module Saml
+  class IdpControllerTest < ActionDispatch::IntegrationTest
+
+    describe "#create" do
+      test "cancel_create_action" do
+        auth_request = Saml::Request::Encoding.encode Saml::Request::Compression.deflate <<~XML
+          <AuthnRequest xmlns="#{Saml::Namespaces::SAMLP}" ID="lol" Version="2.0" IssueInstant="2013-03-18T03:24:19Z">
+          </AuthnRequest>
+        XML
+        post "/saml/auth", params: { SAMLRequest: auth_request, submit: "Cancel" }
+
+
+
+
+        assert_match(/Status: urn:oasis:names:tc:SAML:2.0:status:Responder/, response.body)
+        assert_response :success
+      end
     end
   end
 end

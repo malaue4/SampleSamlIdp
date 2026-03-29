3611d4e79de5b26f79e91597c63feef0c0ef1b31
Martin Laursen <malaue4@gmail.com>
2026-03-29 21:11:55 +0200

    Refactor `StatusResponse` parsing: add raw XML handling, validate against protocol schema, modularize response type logic, and enhance attribute resolution methods.

--- app/models/saml/status_response.rb
+++ app/models/saml/status_response.rb
@@ -6,6 +6,16 @@ module Saml
     include ActiveModel::Attributes
     include ToXml
 
+    RESPONSE_TYPES = %w[
+      Response
+      ArtifactResponse
+      ManageNameIDResponse
+      LogoutResponse
+      NameIDMappingResponse
+    ]
+
+    attr_reader :raw_xml
+
     attribute :id, :string
     attribute :in_response_to, :string
     attribute :version, :string
@@ -19,19 +29,90 @@ module Saml
     attribute :status
 
 
-    # @param [Nokogiri::XML::Node] node
-    def self.parse(node)
-      attributes = {
-        id: node["ID"],
-        in_response_to: node["InResponseTo"],
-        version: node["Version"],
-        issue_instant: node["IssueInstant"],
-        destination: node["Destination"],
-        consent: node["Consent"],
-      }
-      case node.name
-      when "Response" then Response.new(node)
+    # @param [String] raw_xml
+    def self.parse(maybe_encoded_xml)
+      decoded_xml = Encoding.decode_if_needed(maybe_encoded_xml)
+      raw_xml = Compression.inflate_if_needed(decoded_xml)
+
+      document = Nokogiri::XML(raw_xml)
+      errors = Dir.chdir(File.join(Rails.root, "public")) do
+        schema = Nokogiri::XML::Schema(File.read(File.join(Rails.root, "public", "saml-schema-protocol-2.0.xsd")))
+        schema.validate document
       end
+
+      if errors.any?
+        Rails.logger.error "Error validating SAML request: #{errors.join(", ")}"
+        raise Errors::SchemaError, errors.join("\n")
+      end
+
+      response_type_name = document
+               .at_xpath(RESPONSE_TYPES.map { |type| "/samlp:#{type}" }.join("|"), "samlp" => Namespaces::SAMLP).name
+      case response_type_name
+      when "Response" then Response.new(raw_xml:)
+      else
+        raise NotImplementedError, "Unknown response type: #{response_type_name}"
+      end
+    end
+
+
+
+    def initialize(raw_xml:, **attributes)
+      super(attributes)
+      @raw_xml = raw_xml
+    end
+
+    def id
+      @id ||= status_response_element&.[]("ID")
+    end
+
+    def in_response_to
+      @in_response_to ||= status_response_element&.[]("InResponseTo")
+    end
+
+    def version
+      @version ||= status_response_element&.[]("Version")
+    end
+
+    def issue_instant
+      @issue_instant ||= status_response_element&.[]("IssueInstant")
+    end
+
+    def destination
+      @destination ||= status_response_element&.[]("Destination")
+    end
+
+    def consent
+      @consent ||= status_response_element&.[]("Consent")
+    end
+
+    def issuer_element
+      @issuer_element ||= status_response_element&.at_xpath("saml:Issuer", "saml" => Namespaces::SAML)
+    end
+
+    def issuer
+      return if issuer_element.blank?
+
+      @issuer ||= NameId.parse(issuer_element)
+    end
+
+    def status_element
+      @status_element ||= status_response_element&.at_xpath("samlp:Status", "samlp" => Namespaces::SAMLP)
+    end
+
+    def status
+      if status_element.blank?
+        return
+      end
+
+      @status ||= Status.parse(status_element)
+    end
+
+    def document
+      @document ||= Nokogiri::XML(raw_xml)
+    end
+
+    def status_response_element
+      @status_response_element ||= document.at_xpath(RESPONSE_TYPES.map { |type| "/samlp:#{type}" }.join("|"), "samlp" => Namespaces::SAMLP)
     end
 
     private
